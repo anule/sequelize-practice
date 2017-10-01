72fde08b4e18345d04731cbe9cb9a5e85759e59f
Anule Ndukwu <anule.ndukwu@gmail.com>
2017-10-01 17:35:22 -0400

    passing all specs but the getter method

--- models/user.js
+++ models/user.js
@@ -2,9 +2,24 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: { type: Sequelize.STRING },
+  last: { type: Sequelize.STRING },
+  age: { type: Sequelize.INTEGER },
+  email: { type: Sequelize.STRING, allowNull: false },
+  bio: { type: Sequelize.TEXT }
 }, {
-// ...AND HERE
+  validate: {
+    noEmail() {
+      if (!this.email) throw new Error('email cannot be null');
+    },
+    minEighteen() {
+      if (this.age < 18) throw new Error('Validation min on age failed');
+    }
+  }
 });
 
+User.prototype.haveBirthday = function(){
+  this.age = this.age + 1;
+  return this.save();
+};
 module.exports = User;
